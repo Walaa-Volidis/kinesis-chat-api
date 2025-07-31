0350e7b8c1633643d7728e4db53cb56c0329130f
Walaa VOILIDAS <walaavolidis@gmail.com>
2025-07-31 11:38:08 +0300

    remove region name

--- app/kinesisClient.js
+++ app/kinesisClient.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const kinesis = new KinesisClient({
-  region: process.env.AWS_REGION || 'us-east-1',
+  region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
